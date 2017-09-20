f7fc966cac8eacad2b4a0428c6610fca68387cd8
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-09-20 19:55:32 +0800

    默认支持 scss/sass

--- demo/test1.js
+++ demo/test1.js
@@ -4,6 +4,8 @@ const config = require('$config');
 
 const styles1 = require('./assets/index.less');
 const styles2 = require('./assets/test1.css');
+const styles3 = require('./assets/test3.scss');
+
 const test4 = require('./test4.vue');
 
 log('这是 test1');
